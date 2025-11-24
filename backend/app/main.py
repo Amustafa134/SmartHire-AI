9fc8db24b2468886e980138421e90962ca2d7626
Ahmed Mustafa <boskama134@gmail.com>
2025-11-24 03:46:36 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
-from routes import cv_routes, job_routes, match_routes, interview_routes
+#from routes import cv_routes, job_routes, match_routes, interview_routes
 
 app = FastAPI(title="SmartHire AI - MVP")
 
-app.include_router(cv_routes.router)
-app.include_router(job_routes.router)
-app.include_router(match_routes.router)
-app.include_router(interview_routes.router)
+#app.include_router(cv_routes.router)
+#app.include_router(job_routes.router)
+#app.include_router(match_routes.router)
+#app.include_router(interview_routes.router)
 
-@app.get("/")
+@app.get("/Welcome")
 def root():
-    return {"message": "SmartHire AI backend is running"}
+    return {
+        "message": "SmartHire AI backend is running"
+    }
