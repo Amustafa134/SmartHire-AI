9d7352a2cef3c71815ec1f66cc3067f1619b2049
Ahmed Mustafa <boskama134@gmail.com>
2025-11-24 18:22:37 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from app.routes.base import base_router
+#from routes import base
+
+load_dotenv(".env")
+
+
 #from routes import cv_routes, job_routes, match_routes, interview_routes
 
 app = FastAPI(title="SmartHire AI - MVP")
+
 
 #app.include_router(cv_routes.router)
 #app.include_router(job_routes.router)
 #app.include_router(match_routes.router)
 #app.include_router(interview_routes.router)
 
-@app.get("/Welcome")
-def root():
-    return {
-        "message": "SmartHire AI backend is running"
-    }
+app.include_router(base_router)
+
+
+# test_env.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print("ENV:", os.getenv("YOUR_VARIABLE"))
