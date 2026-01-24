279ea0385723d4a18087b1af311ef54ac0a6fc50
Ahmed Mustafa <boskama134@gmail.com>
2026-01-24 16:37:11 +0200

    Create DataController.py

---
+++ src/backend/app/controllers/DataController.py
@@ -0,0 +1,21 @@
+from .BaseController import BaseController
+from fastapi import UploadFile
+
+
+class DataController(BaseController):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.size_scale = 1024 * 1024  # Convert MB to Bytes
+
+    def validate_uploaded_file(self, file: UploadFile):
+
+        if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
+            raise ValueError("Unsupported file type.")
+
+        if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
+            raise ValueError("File size exceeds the maximum limit.")
+        
+        return True
+    
