987f8c5a935e0bad45ff521382277b404325d068
Ahmed Mustafa <boskama134@gmail.com>
2026-01-24 16:37:44 +0200

    Update main.py

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,28 +1,8 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-from app.routes.base import base_router
-#from routes import base
+from backend.app.routes import base, data
 
-load_dotenv(".env")
-
-
-#from routes import cv_routes, job_routes, match_routes, interview_routes
 
 app = FastAPI(title="SmartHire AI - MVP")
 
-
-#app.include_router(cv_routes.router)
-#app.include_router(job_routes.router)
-#app.include_router(match_routes.router)
-#app.include_router(interview_routes.router)
-
-app.include_router(base_router)
-
-
-# test_env.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print("ENV:", os.getenv("YOUR_VARIABLE"))
+app.include_router(base.base_router)
+app.include_router(data.data_router)
