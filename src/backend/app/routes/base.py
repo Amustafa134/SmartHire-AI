77d655e5b875e0b51d7206a81651612736853ab0
Ahmed Mustafa <boskama134@gmail.com>
2026-01-24 16:37:04 +0200

    Update base.py

--- src/backend/app/routes/base.py
+++ src/backend/app/routes/base.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
+from backend.app.helpers.config import get_settings, Settings
+
 import os
+
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -7,9 +10,12 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome():
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
+async def welcome(app_settings: Settings = Depends(get_settings)):
+
+    #app_settings = get_settings()
+
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
 
     return {
         "message": f"Welcome to {app_name}!",
